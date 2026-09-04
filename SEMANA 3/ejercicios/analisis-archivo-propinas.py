559ea89ab4736dfac9211ba19ff67915065b8a08
LucasChas <luquitaschasdiaz3@gmail.com>
2026-09-04 13:48:00 -0300

    Actividades archivos

---
+++ SEMANA 3/ejercicios/analisis-archivo-propinas.py
@@ -0,0 +1,17 @@
+# El archivo tips.csv contiene los datos de las propinas recibidas por los emplea-
+# dos de una pizzería en New York.
+
+# El archivo contiene los siguientes campos separados por comas
+# • total_bill: Importe total del servicio
+# • tip: Importe de la propina
+# • sex: Sexo del cliente
+# • smoker: El cliente es fumador (Yes, No)
+# • day: Día (Juev, Vie, Sab, Dom)
+# • time: Horario (Almuerzo, Cena)
+# • size: Tamaño de la mesa (comensales)
+# Se requiere desarrollar un programa que desde los datos del archivo
+# informe:
+# • ¿Quién paga más propinas?¿Hombres o mujeres?
+# • ¿Cuáles son los días más ‘lentos’, con menos propinas?
+# • ¿Cuál es el promedio de las propinas?
+# • ¿Cuándo se vendió la orden más grande (qué día y en qué turno)?
---
+++ SEMANA 3/ejercicios/lectura-csv-cp.py
@@ -0,0 +1,19 @@
+# Lectura de un csv con códigos postales
+# El archivo cp.csv contiene el listado de los códigos postales de tres provincias
+# argentinas. En cada línea se encuentran, separados por un símbolo de punto
+# y coma (;) los siguientes datos:
+# • Provincia: representada por una letra mayúscula según el estándar ISO
+# correspondiente.
+
+# • Código: es un número entero de cuatro dígitos que identifica una locali-
+# dad o varias localidades vecinas. Puede estar repetido.
+
+# • Nombre: es el nombre de la localidad correspondiente a ese código.
+# Se requiere leer todo el contenido del archivo y guardarlo en una lista que
+# contenga un elemento por cada línea. En cada elemento debe almacenarse
+# alguna estructura de datos que permita acceder individualmente a cada dato
+# que conforma un codigo postal.
+# Luego de la carga el programa debe permitir el ingreso de uno o más
+# códigos numéricos y listar la provincia y nombre de todas las localidades
+# asignadas a dichos códigos
+
